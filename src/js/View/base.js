19008f1e451eb9bc3dd62681e6aef4c82ccc86a4
Mohamed <mohamed.ahmed9833@gmail.com>
2020-04-29 20:02:59 +0700

    search view part 1

---
+++ src/js/View/base.js
@@ -0,0 +1,5 @@
+export const elements = {
+    searchForm: document.querySelector('.search'),
+    searchField: document.querySelector('.search__field'),
+    searchList:document.querySelector('.results__list')
+}
