3f6d1bbc48337c6b76064a6e35859b0541745172
Mohamed Desouky <mohamed.and9833@gmail.com>
2020-07-08 14:08:44 +0700

    Refactoring All Controllers in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -12,35 +12,25 @@ import {
 import Recipe from "./models/recipe";
 import Likes from "./models/Like";
 
-/*
- * Global state for the app
- * 1- search object
- * 2-current recipes
- * 3-shopping list
- * 4-liked recipes
- */
+
 const state = {};
 elements.searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     controlSearch();
 });
 const controlSearch = async () => {
-    //1) get query from the view
+  
     const query = SearchView.getInput();
 
     if (query) {
-        //2) new search object and add it to the state
+     
         state.search = new Search(query);
 
-        //prepare the UI
         SearchView.clearInput();
         renderLoader(elements.searchResult);
         SearchView.clearResults();
 
-        //4) search for recipes
         await state.search.getResult();
-
-        // 5) render result in UI
         removeLoader();
 
         SearchView.renderResults(state.search.result);
@@ -53,7 +43,6 @@ const controlSearch = async () => {
 elements.Button.addEventListener("click", (e) => {
     const btn = e.target.closest(".btn-inline");
     const goToPage = parseInt(btn.dataset.goto, 10);
-    //SearchView.clearBtn();
     SearchView.clearResults();
 
     SearchView.renderResults(state.search.result, goToPage);
@@ -61,28 +50,25 @@ elements.Button.addEventListener("click", (e) => {
 
 
 const controlRecipe = async () => {
-    // get the id from the URl
+
     const id = window.location.hash.replace("#", "");
     console.log(id);
 
     if (id) {
-        //prepare UI for changes
+
         renderLoader(elements.recipe);
         if (state.search) SearchView.highLighted(id);
 
-        //create new recipe object
         state.recipe = new Recipe(id);
 
-        //get recipe from data
         await state.recipe.getRecipe();
 
         state.recipe.parseIndredients();
 
-        //calculate time and serving
         state.recipe.calcTime();
         state.recipe.calcServings();
 
-        //RenderRecipe
+
         removeLoader();
         RecipeView.clearOldResult();
         ListView.clearOldResult();
@@ -97,13 +83,11 @@ const controlRecipe = async () => {
 window.addEventListener('load', () => {
     state.likes = new Likes();
 
-    // Restore likes
     state.likes.readStorage();
 
-    // Toggle like menu button
     LikeView.toggleLikeMenu(state.likes.getNumLikes());
 
-    // Render the existing likes
+
     state.likes.likes.forEach(like => LikeView.renderLike(like));
 });
 
@@ -133,10 +117,10 @@ const controlLike = () => {
     }
     LikeView.toggleLikeMenu(state.likes.getNumLikes());
 }
-// handle recipe button click
+
 elements.recipe.addEventListener('click', e => {
 
-    //Decrese button was clicked
+
     if (e.target.matches('.btn-decrese , .btn-decrese *')) {
         if (state.recipe.servings > 1) {
             state.recipe.updateServings('dec');
@@ -144,7 +128,7 @@ elements.recipe.addEventListener('click', e => {
 
         }
 
-        //Decrese button was clicked
+
     } else if (e.target.matches('.btn-increse , .btn-increse *')) {
         state.recipe.updateServings('inc');
         RecipeView.updateServingsIngredients(state.recipe);
@@ -155,13 +139,12 @@ elements.recipe.addEventListener('click', e => {
     }
 })
 
-//window.l = new List();
+
 
 const controlList = () => {
-    // create a new list if there is none yet
+
     if (!state.List) state.list = new List();
 
-    // add each ingredients to the list and UI
     state.recipe.ingredients.forEach(el => {
         const item = state.list.addItem(el.count, el.unit, el.ingredient);
         ListView.renderItem(item);
