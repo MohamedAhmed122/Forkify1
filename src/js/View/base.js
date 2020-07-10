91aef2589d3ae4a185b331438d94b668923781ea
Mohamed Desouky <mohamed.and9833@gmail.com>
2020-07-10 15:54:37 +0700

    Update base.js

--- src/js/View/base.js
+++ src/js/View/base.js
@@ -10,22 +10,3 @@ export const elements = {
     likesMenu: document.querySelector('.likes__field'),
     likesList: document.querySelector('.likes__list'),
 };
-export const elementStrings = {
-    loader: 'loader'
-};
-export const renderLoader = parent => {
-    const loader = `
-    <div class="${elementStrings.loader}">
-        <svg>
-            <use href="img/icons.svg#icon-cw"></use>
-        </svg>
-    </div>
-`;
-    parent.insertAdjacentHTML('afterbegin', loader);
-}
-export const removeLoader = () => {
-    const loader = document.querySelector(`.${elementStrings.loader}`);
-    if (loader) {
-        loader.parentElement.removeChild(loader);
-    }
-}
