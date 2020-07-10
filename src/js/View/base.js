e896da1d125f331add84453f92c5fe645313d92b
Mohamed Desouky <mohamed.and9833@gmail.com>
2020-07-10 20:53:12 +0700

    Adding new recipes

--- src/js/View/base.js
+++ src/js/View/base.js
@@ -10,3 +10,22 @@ export const elements = {
     likesMenu: document.querySelector('.likes__field'),
     likesList: document.querySelector('.likes__list'),
 };
+export const elementStrings = {
+    loader: 'loader'
+};
+export const renderLoader = parent => {
+    const loader = `
+    <div class="${elementStrings.loader}">
+        <svg>
+            <use href="img/icons.svg#icon-cw"></use>
+        </svg>
+    </div>
+`;
+    parent.insertAdjacentHTML('afterbegin', loader);
+}
+export const removeLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    if (loader) {
+        loader.parentElement.removeChild(loader);
+    }
+}
